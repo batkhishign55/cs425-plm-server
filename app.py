39da460fb0238043daaeef2f441878d701c260ba
batkhishig <batkhishign55@gmail.com>
2023-10-24 12:08:19 -0500

    cleaned up, added navbar elements

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
-from bps.cart.route import cart
 from bps.lot.route import lot
 from bps.user.route import user
-from bps.product.route import product
 from db import init_db
 
 app = Flask(__name__)
 
 app.register_blueprint(user)
-app.register_blueprint(cart)
-app.register_blueprint(product)
 app.register_blueprint(lot)
 
 with app.app_context():
