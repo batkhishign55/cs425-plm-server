f7c720e20464dcb5813eadc21c33aa5b192a0740
AnanyaMMenon <amenon13@hawk.iit.edu>
2023-10-27 18:05:24 -0500

    Updated parking logs

---
+++ bps/log/controller.py
@@ -0,0 +1,18 @@
+from .model import log
+
+
+class logController:
+    def __init__(self):
+        self.model = log()
+
+    def get(self):
+        data = self.model.get()
+        if not data:
+            return {'status': False, 'data': data, 'message': 'Data is not found'}
+        return {'status': True, 'data': data, 'message': ''}
+
+    def getSinglelog(self, log_id):
+        data = self.model.getSinglelog(log_id)
+        if not data:
+            return {'status': False, 'data': data, 'message': 'Data is not found'}
+        return {'status': True, 'data': data, 'message': ''}
---
+++ static/LogUI.js
@@ -0,0 +1,47 @@
+window.addEventListener("load", function () {
+    new Api().callApilogs().then(new UI().showlogs);
+});
+
+class Api {
+async callApilogs() {
+    const resp = await fetch("/log/api/");
+    const jsonresp=await resp.json()
+    return jsonresp;
+}
+}
+
+class UI {
+constructor() {
+    this.showlogs = this.showlogs.bind(this);
+}
+
+loadPage(page) {
+    let productListElement = document.getElementById("logListId");
+    productListElement.innerHTML = page;
+}
+
+showlogs(apiresp) {
+    let logRows = [];
+    for (let log of apiresp.data) {
+    let row = this.createRow(log);
+    logRows.push(row);
+    }
+
+    this.loadPage(logRows.join(""));
+}
+
+createRow(log) {
+    const cardTemplate = `
+        <tr>
+            <th scope="row">${log[0]}</th>
+            <td>${log[1]}</td>
+            <td>${log[2]}</td>
+            <td>${log[4]}</td>
+            <td>${log[3]}</td>
+            <td>${log[5]}</td>
+            
+        </tr>
+        `;
+    return cardTemplate;
+}
+}
