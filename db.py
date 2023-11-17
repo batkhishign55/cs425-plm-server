e8c761c57c60ecf95d7280ca2e9ea6c3a75524ce
AnanyaMMenon <147334173+AnanyaMMenon@users.noreply.github.com>
2023-11-17 00:23:47 -0600

    Update db.py

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ def init_db():
     g.db = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Adhya@2007#",
+        password="password",
         database="plm"
     )
 
